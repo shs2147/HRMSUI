3a5d4060d0881e21b3a540ca903951ec58f4e043
piyush7827 <100812764+piyush7827@users.noreply.github.com>
2023-01-13 18:27:25 +0530

    Dashboard

--- src/App.js
+++ src/App.js
@@ -91,7 +91,7 @@ function App() {
     // <div>
     //   <BrowserRouter>
     //     <Routes>
-    //     <Route path="/" element={<SignIn handler={handler} />} exact />
+    //       <Route path="/Dashboard" element={<Dashboard />} exact />
     //     </Routes>
     //   </BrowserRouter>
     // </div>
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,23 +1,58 @@
-import React from 'react'
-import Card from '././Dashboard/Card/Card'
-import pic from '../components/img/pic.png'
-import Calender from './Dashboard/Calender/Calender'
-import DasNav from "./Dashboard/Navbar/DasNav"
-
+import React, { useEffect, useState } from "react";
+import Card from "././Dashboard/Card/Card";
+import pic from "../components/img/pic.png";
+import Calender from "./Dashboard/Calender/Calender";
+import DasNav from "./Dashboard/Navbar/DasNav";
+import "./dashboard.css";
+import { NavItem } from "react-bootstrap";
 
 function Dashboard() {
+  const [data, setData] = useState([]);
+  let ln = 0;
+  const options = { method: "GET" };
+  useEffect(() => {
+    fetch("http://localhost:8080/basic/fetchdata", options)
+      .then((response) => response.json())
+      .then((response) => setData(response))
+
+      .catch((err) => console.error(err));
+  }, []);
+
+  data.forEach(myFunction);
+
+  function myFunction(item) {
+    ln++;
+  }
+  console.log(data);
+  // const ln = setData.length;
+  console.log(ln);
   return (
-    <div style={{maxwidth:'100%'}}>
+    <div style={{ maxwidth: "100vw" }}>
       <DasNav />
-      <div className='justify-content-around ' style={{ flexDirection: "row", display: 'flex',maxWidth:'fit-content' }}>
-        <Card img={pic} title="Permanent Employee" number="2" />
+
+      <div
+        className="carddisplay"
+        style={{
+          flexDirection: "row",
+          display: "flex",
+          flexWrap: "wrap",
+          objectFit: "contain",
+        }}
+      >
+        <Card img={pic} title="Permanent Employee" number={ln} />
         <Card img={pic} title="Under Probation Employee" number="2" />
         {/* <Card img={pic} title="Permanent Employee" number="2" /> */}
       </div>
+
       <Calender />
+      {/* {data.map((item) => (
+        <tr>
+          <td>{item.employeeId}</td>
+          
+        </tr>
+      ))} */}
     </div>
-  )
+  );
 }
 
-
-export default Dashboard
+export default Dashboard;
